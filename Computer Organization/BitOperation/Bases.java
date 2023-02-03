7146d6c571696a6c66d76f75a461af649c733556
Wonho Lim <62059099+wonholim02@users.noreply.github.com>
2023-02-02 21:28:46 -0500

    Add files via upload

--- Computer Organization/BitOperation/Bases.java
+++ Computer Organization/BitOperation/Bases.java
@@ -1,13 +1,5 @@
 public class Bases
 {
-    /**
-     * Convert a string containing ASCII characters (in binary) to an int.
-     *
-     * You do not need to handle negative numbers. The Strings we will pass in
-     * will be valid binary numbers, and able to fit in a 32-bit signed integer.
-     *
-     * Example: binaryStringToInt("111"); // => 7
-     */
     public static int binaryStringToInt(String binary) {
         int stack = 0;
         int ans = 0;
@@ -22,16 +14,6 @@ public class Bases
         return ans;
     }
 
-    /**
-     * Convert a string containing ASCII characters (in decimal) to an int.
-     *
-     * You do not need to handle negative numbers. The Strings we will pass in
-     * will be valid decimal numbers, and able to fit in a 32-bit signed integer.
-     *
-     * Example: decimalStringToInt("46"); // => 46
-     *
-     * You may use multiplication in this method.
-     */
     public static int decimalStringToInt(String decimal) {
         int ans = 0;
         for (int i = 0; i < decimal.length(); i++) {
@@ -40,14 +22,6 @@ public class Bases
         return ans;
     }
 
-    /**
-     * Convert a string containing ASCII characters (in hex) to an int.
-     * The input string will only contain numbers and uppercase letters A-F.
-     * You do not need to handle negative numbers. The Strings we will pass in will be
-     * valid hexadecimal numbers, and able to fit in a 32-bit signed integer.
-     *
-     * Example: hexStringToInt("A6"); // => 166
-     */
     public static int hexStringToInt(String hex) {
         int stack = 0;
         int ans = 0;
@@ -63,17 +37,6 @@ public class Bases
         return ans;
     }
 
-    /**
-     * Convert a int into a String containing ASCII characters (in octal).
-     *
-     * You do not need to handle negative numbers.
-     * The String returned should contain the minimum number of characters
-     * necessary to represent the number that was passed in.
-     *
-     * Example: intToOctalString(166); // => "246"
-     *
-     * You may declare one String variable in this method.
-     */
     public static String intToOctalString(int octal) {
         String ans = "";
         int remainder = 0;
@@ -88,20 +51,6 @@ public class Bases
         return ans;
     }
 
-    /**
-     * Convert a String containing ASCII characters representing a number in
-     * binary into a String containing ASCII characters that represent that same
-     * value in hex.
-     *
-     * The output string should only contain numbers and capital letters.
-     * You do not need to handle negative numbers.
-     * All binary strings passed in will contain exactly 32 characters.
-     * The hex string returned should contain exactly 8 characters.
-     *
-     * Example: binaryStringToHexString("00001111001100101010011001011100"); // => "0F32A65C"
-     *
-     * You may declare one String variable in this method.
-     */
     public static String binaryStringToHexString(String binary) {
         String ans = "";
         int binaryNum  = 0;
--- Computer Organization/BitOperation/BitVector.java
+++ Computer Organization/BitOperation/BitVector.java
@@ -1,72 +1,27 @@
 public class BitVector
 {
-    /**
-     * 32-bit data initialized to all zeros. Here is what you will be using to
-     * represent the Bit Vector. Do not change its scope from private.
-     */
     private int bits;
 
-    /** You may not add any more fields to this class other than the given one. */
-
-    /**
-     * Sets the bit (sets to 1) pointed to by index.
-     * @param index index of which bit to set.
-     *              0 for the least significant bit (right most bit).
-     *              31 for the most significant bit.
-     */
     public void set(int index) {
         this.bits = this.bits | (1 << index);
     }
 
-    /**
-     * Clears the bit (sets to 0) pointed to by index.
-     * @param index index of which bit to set.
-     *              0 for the least significant bit (right most bit).
-     *              31 for the most significant bit.
-     */
     public void clear(int index) {
         this.bits = this.bits & ~(1 << index);
     }
 
-    /**
-     * Toggles the bit (sets to the opposite of its current value) pointed to by
-     * index.
-     * @param index index of which bit to set.
-     *              0 for the least significant bit (right most bit).
-     *              31 for the most significant bit.
-     */
     public void toggle(int index) {
         this.bits = this.bits ^ (1 << index);
     }
 
-    /**
-     * Returns true if the bit pointed to by index is currently set.
-     * @param index index of which bit to check.
-     *              0 for the least significant bit (right-most bit).
-     *              31 for the most significant bit.
-     * @return true if the bit is set, false if the bit is clear.
-     *         If the index is out of range (index >= 32), then return false.
-     */
     public boolean isSet(int index) {
         return (!(index >= 32) && ((1 << index) == (this.bits & (1 << index))));
     }
 
-    /**
-     * Returns true if the bit pointed to by index is currently clear.
-     * @param index index of which bit to check.
-     *              0 for the least significant bit (right-most bit).
-     *              31 for the most significant bit.
-     * @return true if the bit is clear, false if the bit is set.
-     *         If the index is out of range (index >= 32), then return true.
-     */
     public boolean isClear(int index) {
         return ((index >= 32) || ((1 << index) != (this.bits & (1 << index))));
     }
 
-    /**
-     * Returns the number of bits currently set (=1) in this bit vector.
-     * You may use the ++ operator to increment your counter.
-     */
     public int onesCount() {
         int cnt = 0;
         for (int i = 0; i < 32; i++) {
@@ -77,10 +32,6 @@ public class BitVector
         return cnt;
     }
 
-    /**
-     * Returns the number of bits currently clear (=0) in this bit vector.
-     * You may use the ++ operator to increment your counter.
-     */
     public int zerosCount() {
         int cnt = 0;
         for (int i = 0; i < 32; i++) {
@@ -90,14 +41,7 @@ public class BitVector
         }
         return cnt;
     }
-
-    /**
-     * Returns the "size" of this BitVector. The size of this bit vector is
-     * defined to be the minimum number of bits that will represent all of the
-     * ones.
-     *
-     * For example, the size of the bit vector 00010000 will be 5.
-     */
+    
     public int size() {
         int size = 0;
         for (int i = 31; i >= 0; i--) {
