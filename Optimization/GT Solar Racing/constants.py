cb2dd266f8fa339a774d1cfb710eb55cb9571f4b
Wonho Lim <62059099+wonholim02@users.noreply.github.com>
2023-02-28 14:35:07 -0500

    deleting

--- Optimization/GT Solar Racing/constants.py
+++
@@ -1,7 +0,0 @@
-rho = 1.225
-Ta = 298.15
-g = 9.81
-CC = 40 #%cloud cover fraction [0,100]
-dx_motSpins = 1 #%spins of the motor corresponding to a discrete increment
-max_insol = 1050 #%1.5AM conditions
-iter_num = 10 #%target charge convergence iterations
